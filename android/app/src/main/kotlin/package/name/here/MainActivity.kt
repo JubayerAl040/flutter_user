e69a5617feacca121d7d52031c867945e4d8cf20
jubayer040 <135593620+JubayerAl040@users.noreply.github.com>
2024-04-28 17:51:08 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/package/name/here/MainActivity.kt
@@ -0,0 +1,7 @@
+package package name here
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
